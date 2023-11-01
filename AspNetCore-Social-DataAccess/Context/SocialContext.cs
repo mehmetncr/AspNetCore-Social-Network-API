674c5f1a40f0046790e1a0bcccdb020e8a642408
mehmetncr <mehmet.ncar@gmail.com>
2023-11-01 12:16:44 +0300

    Add project files.

---
+++ AspNetCore-Social-DataAccess/Context/SocialContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Social_DataAccess.Context
+{
+    public class SocialContext : DbContext
+    {
+        public SocialContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
